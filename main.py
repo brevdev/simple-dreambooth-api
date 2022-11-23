c97d7117179db477abdeb6451b489f057771fe6b
samlhuillier1 <sam.jansa123@gmail.com>
2022-11-23 04:30:53 +0000

    working main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from fastapi import FastAPI, File, UploadFile, status
 from fastapi.exceptions import HTTPException
 import aiofiles
 import os 
+import zipfile
+import shutil
+
 
 app = FastAPI()
 
@@ -23,93 +26,97 @@ async def root():
 
 CHUNK_SIZE = 1024 * 1024  # adjust the chunk size as desired
 
+temporaryZipFileDirectory = './userzipfiles/'
+
+if not os.path.exists(temporaryZipFileDirectory):
+    os.makedirs(temporaryZipFileDirectory)
+
+extractedFilesDirectory = './extractedfiles/'
+if not os.path.exists(extractedFilesDirectory):
+    os.makedirs(extractedFilesDirectory)
+
+outputModelsDirectory = './outputmodels/'
+if not os.path.exists(outputModelsDirectory):
+    os.makedirs(outputModelsDirectory)
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
-    # convert file to bytes
     
-    # print("file is:")
-    # print(file)
-    # task = train.delay(data)
-    # create a uuid for the user request
-    # uuid = uuid4()
-    # # # check if directory "userzipfiles" exists, if not create it
-    # if not os.path.exists("./userzipfiles"):
-    #     os.makedirs("./userzipfiles")
-    # try:
-    #     print("abc")
-    #     filepath = './userzipfiles/' + str(uuid) + '.zip'
-    #     print(filepath)
-    #     async with aiofiles.open(filepath, 'wb') as f:
-    #         while chunk := await file.read(CHUNK_SIZE):
-    #             await f.write(chunk)
-    # except Exception:
-    #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
-    #         detail='There was an error uploading the file')
-    # finally:
-    #     await file.close()
-    # # print("filepath:")
-    # # print(filepath)
-    # dataDirectory = 'unzippedfiles/' + str(uuid)
-    # if not os.path.exists("./unzippedfiles"):
-    #     os.makedirs("./unzippedfiles")
-    # import zipfile
-    # with zipfile.ZipFile(filepath, 'r') as zip_ref:
-    #     zip_ref.extractall(dataDirectory)
-    # # # delete the zip file
-    # # # os.remove(filepath)
-    # print("BEFORE TASK")
-    # task = divide.delay(1,2)
-    images = await file.read()
-    byte = base64.b64encode(images)
-    data = {
-            'image': byte.decode('utf-8'),
-        }
-    task = train.delay(data)
+    uuid = uuid4()
+    
+    try:
+        zipFilepath = temporaryZipFileDirectory + str(uuid) + '.zip' # todo: could add a path.join thing here
+        print(zipFilepath)
+        async with aiofiles.open(zipFilepath, 'wb') as f:
+            while chunk := await file.read(CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail='There was an error uploading the file')
+    finally:
+        await file.close()
+    
+    # extract the zip file then delete it
+    dataDirectory = extractedFilesDirectory + str(uuid)
+    if not os.path.exists(dataDirectory):
+        os.makedirs(dataDirectory)
+    with zipfile.ZipFile(zipFilepath, 'r') as zip_ref:
+        zip_ref.extractall(dataDirectory)
+    # find all .jpg files in the dataDirectory
+
+    finalOutputDirectory = './finaldatadirectory/' + str(uuid)
+    if not os.path.exists(finalOutputDirectory):
+        os.makedirs(finalOutputDirectory)
+    jpgFiles = []
+    for root, dirs, files in os.walk(dataDirectory):
+        for file in files:
+            if file.endswith(".jpg"):
+                filePath = os.path.join(root, file)
+                shutil.move(filePath, finalOutputDirectory)
+                # move filePath to finalOutputDirectory
+            
+
+    os.remove(zipFilepath)
+
+
+    outputDirectory = outputModelsDirectory + str(uuid)
+    if not os.path.exists(outputDirectory):
+        os.makedirs(outputDirectory)
+    # then now we can run the actual worker job
+
+    print("Final output directory: " + finalOutputDirectory)
+    
+    task = train.delay(finalOutputDirectory, outputDirectory)
     return {"message": f"Successfuly uploaded "}
 
 
 
 @celery.task#(serializer='pickle')
-def train(data):
-    print("IN TRAIN")
-    print(data)
-    import os
-    outputFolder = "test-output-folder"
-    dataDirectory = "unzippedfiles/f9b98410-8e37-469e-99f5-6dc6eb0b7105"
-    # uuid = uuid4()
-    # # check if directory "userzipfiles" exists, if not create it
-    # if not os.path.exists("./userzipfiles"):
-    #     os.makedirs("./userzipfiles")
-    # try:
-    #     print("abc")
-    #     filepath = './userzipfiles/' + str(uuid) + '.zip'
-    #     print(filepath)
-    #     async with aiofiles.open(filepath, 'wb') as f:
-    #         while chunk := await file.read(CHUNK_SIZE):
-    #             await f.write(chunk)
-    # except Exception:
-    #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
-    #         detail='There was an error uploading the file')
-    # finally:
-    #     await file.close()
-    # f = open('output.zip', 'wb')
-    # # save file bytes to zip file
-    # f.write(file)
-    # f.close()
-#     print("LHUILLIER STARTING TASK")
-#     os.system(f"accelerate launch train_dreambooth.py \
-#   --pretrained_model_name_or_path='CompVis/stable-diffusion-v1-4'  \
-#   --instance_data_dir={dataDirectory} \
-#   --output_dir={outputFolder} \
-#   --instance_prompt='photo of sks dog' \
-#   --resolution=512 \
-#   --train_batch_size=1 \
-#   --gradient_accumulation_steps=1 \
-#   --learning_rate=5e-6 \
-#   --lr_scheduler='constant' \
-#   --lr_warmup_steps=0 \
-#   --max_train_steps=800")
+def train(dataDirectory, outputDirectory):
+    print("running command: \n", f"accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path='CompVis/stable-diffusion-v1-4'  \
+  --instance_data_dir={dataDirectory} \
+  --output_dir={outputDirectory} \
+  --instance_prompt='photo of sks dog' \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler='constant' \
+  --lr_warmup_steps=0 \
+  --max_train_steps=800")
+    os.system(f"accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path='CompVis/stable-diffusion-v1-4'  \
+  --instance_data_dir={dataDirectory} \
+  --output_dir={outputDirectory} \
+  --instance_prompt='photo of sks dog' \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler='constant' \
+  --lr_warmup_steps=0 \
+  --max_train_steps=800")
 
 async def saveZipFile(file):
     uuid = uuid4()
